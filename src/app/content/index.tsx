b782b8d4a475de6be84ddf7241c15ead9574d072
chenleialan <chenleiaixinlang@sina.cn>
2019-03-26 21:45:20 +0800

    code

--- src/app/content/index.tsx
+++ src/app/content/index.tsx
@@ -1,13 +1,9 @@
 /**
  * Created by chenlei on 2018/7/12.
  */
-import * as React from "react"
-import {Component} from "react"
-import {Form, Input,Select, Icon, Tabs, message,Row, Col, Checkbox, Button} from 'antd'
+import React,{Component} from "react"
+import {Button} from 'antd'
 import './index.scss'
-
-const FormItem = Form.Item;
-const Option = Select.Option;
 
 export default class Content extends Component<any,any> {
     render() {
